931ec1d93d136298717f68184da7ce23a33fe54f
Liza Qandah <55685416+LizaQandah@users.noreply.github.com>
2019-11-09 19:09:17 +0200

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+class helloworld 
+{ 
+    // Your program begins with a call to main().
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello World from liza container"); 
+    } 
+} 
