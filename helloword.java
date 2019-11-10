9222563fcc4f1a927d05b784978837da638da83d
Liza Qandah <55685416+LizaQandah@users.noreply.github.com>
2019-11-10 15:38:08 +0200

    Delete helloword.java

--- helloword.java
+++
@@ -1,8 +0,0 @@
-class helloworld 
-{ 
-    // Your program begins with a call to main().
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello World from liza container"); 
-    } 
-} 
